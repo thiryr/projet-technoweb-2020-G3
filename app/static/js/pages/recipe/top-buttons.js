90369e30b1900f51392b77a232b9310a67ed57ac
thiryr <renaud.thiry@student.unamur.be>
2020-12-18 13:02:48 +0100

    fixes top buttons.

--- app/static/js/pages/recipe/top-buttons.js
+++ app/static/js/pages/recipe/top-buttons.js
@@ -9,24 +9,23 @@ var recipe_id = parseInt(recipe_id_str)
 //switch visibility
 $(document).ready(function() {
 
-    var is_public = $("#visibility").val() === "public"
     $("#visibility").click(function() {
         //'public' is the value to be set
-        $.post("/api/recipe/switch_visibility", { 'recipe_id': recipe_id, 'public': !is_public }).done(function(expected_json) {
+        $.post("/api/recipe/update_visibility", { 'recipe_id': recipe_id }).done(function(expected_json) {
                 //remove or add lock, modify button
-                if (expected_json.public == false) {
-                    $(".info").find(".fa-lock").remove()
+                expected_json = JSON.parse(expected_json)
 
-                    $("#visibility").value("public")
-                    $("#visibility").html("Rendre privé")
-                } else {
+                if (expected_json.public == false) {
                     $($(".info").children(".row")[0]).append('<i class="fa fa-lock margin-right"></i>')
 
-                    $("#visibility").value("public")
-                    $("#visibility").html("Rendre privé")
-                }
+                    $("#visibility").attr("value", "private")
+                    $("#visibility").html("Rendre public")
+                } else {
+                    $(".info").find(".fa-lock").remove()
 
-                is_public = expected_json.public
+                    $("#visibility").attr("value", "public")
+                    $("#visibility").html("Rendre privée")
+                }
 
             })
             .fail(function() {
@@ -35,21 +34,47 @@ $(document).ready(function() {
             })
 
     });
-})
 
-//detele
-$(document).ready(function() {
+    //detele
 
     $("#delete").click(function() {
 
         $.post("/api/recipe/remove", { 'recipe_id': recipe_id }).done(function(expected_json) {
-                location.href = "../index"
+                window.location.href = "/"
             })
             .fail(function() {
                 //do nothing for now
             })
 
     });
-})
 
-//TODO pin
+    //pin
+
+
+    const path_url = window.location.href.split('/')
+    const id = path_url[path_url.length - 1].split('?')[0]
+
+    let target_url = "/api/recipe/switch_pin"
+
+    $("#pin").click(function() {
+        //'public' is the value to be set
+        $.post(target_url, { 'recipe_id': id }).done(function(expected_json) {
+                //remove or add lock, modify button
+                expected_json = JSON.parse(expected_json)
+                if (expected_json.pinned == false) {
+                    $("#pin").attr("value", "unpinned")
+                    $("#pin").html("Épingler")
+                } else {
+                    $("#pin").attr("value", "pinned")
+                    $("#pin").html("Désépingler")
+                }
+
+
+            })
+            .fail(function() {
+
+                //do nothing for now
+            })
+
+    });
+})
