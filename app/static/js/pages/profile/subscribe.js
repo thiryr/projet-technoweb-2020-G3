2ad96876908397d6c34b6af35f85d63fb3b9a421
thiryr <renaud.thiry@student.unamur.be>
2020-12-18 16:48:34 +0100

    fixes subscription, should be last fix

--- app/static/js/pages/profile/subscribe.js
+++ app/static/js/pages/profile/subscribe.js
@@ -1,20 +1,20 @@
 const sab = "S'abonner"
-const desab = "Se désabonner"
+const desab = "Abonné"
 
 
-const create_url = "../api/subscription/create"
-const remove_url = "../api/subscription/remove"
+const create_url = "/api/subscription/create"
+const remove_url = "/api/subscription/remove"
 
-jQuery(function () {
+jQuery(function() {
     const path_url = window.location.href.split('/');
     const id = path_url[path_url.length - 1].split('?')[0];
 
-    let current_state = $("#subscribe-button").children("button").html();
-    let target_url = current_state === sab ? remove_url : create_url;
+    let current_state = $("#subscribe-button").children("button").html().trim();
+    let target_url = current_state === sab ? create_url : remove_url;
 
-    $("#subscribe-button").children("button").on("click", function () {
+    $("#subscribe-button").children("button").on("click", function() {
 
-        $.post(target_url, { 'id': id }).done(function () {
+        $.post(target_url, { 'id': id }).done(function() {
 
             // Toggle state
             current_state = (current_state === sab ? desab : sab);
@@ -22,16 +22,14 @@ jQuery(function () {
             // Swap classes
             if (current_state === sab) {
                 $("#subscribe-button").removeClass('subscribed');
-            }
-            else {
+                target_url = remove_url
+            } else {
                 $("#subscribe-button").addClass('subscribed');
+                target_url = create_url;
             }
-            
+
             // Update text
             $("#subscribe-button").children("button").html(current_state);
-
-            // Update target url
-            target_url = target_url === create_url ? remove_url : create_url;
         })
     })
 
